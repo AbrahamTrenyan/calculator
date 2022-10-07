28d2b89b208265e2c5940313ecbd2e4143de03b1
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-10-07 10:02:56 -0300

    update calculator version

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, unmountComponentAtNode, screen } from "react-dom";
 import userEvent from '@testing-library/user-event';
+import { act } from "react-dom/test-utils";
 import Screen from '../src/Components/Screen/Screen.jsx';
 import App from './App.js';
 const NUMBER_OF_BUTTONS = 19
-test('Should display a 0 on screen at the beggining', () => {
+ test('Should display a 0 on screen at the beggining', () => {
   render(<Screen />);
   const result = screen.getByTestId('result');
   expect(result).toHaveTextContent('0');
@@ -20,4 +21,7 @@ test('Should add number to operation when clicked', () => {
   const calc = screen.getByTestId('calc');
   userEvent.click(number);
   expect(calc).toHaveTextContent(1);
-});
+}); 
+
+
+
